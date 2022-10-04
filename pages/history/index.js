ef78b575df78f3addb2c13ac725e6da9e2e766de
Daniel Brenners <danielbrenners@gmail.com>
2022-10-04 02:24:31 -0400

    add dots and italy to history page

--- pages/history/index.js
+++ pages/history/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "./styles.module.css";
+import PulseDot from "../../components/PulseDot";
 
 export default function Home() {
   return (
@@ -11,8 +12,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className={styles.heading}>Item History</h1>
+      <PulseDot style={{ top: "100px", left: "90px" }} />
+      <PulseDot style={{ top: "220px", left: "170px" }} />
+      <PulseDot style={{ top: "240px", left: "210px" }} />
+      <PulseDot style={{ top: "330px", left: "190px" }} />
+
       <div className={styles.img}>
-        <Image src="" width={327} height={253} />
+        <Image src="/italy-final.jpg" width={327} height={332} />
       </div>
       <div className={styles.article}>
         <div className={styles.heading}>Materials</div>
