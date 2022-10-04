7d53847a98f7b1b65a1c9961d1652624299b250c
Daniel Brenners <danielbrenners@gmail.com>
2022-10-04 01:48:28 -0400

    add preorder animation

--- pages/preorder/index.js
+++ pages/preorder/index.js
@@ -1,8 +1,28 @@
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "./styles.module.css";
+import { useSpring, animated, config } from "react-spring";
 
 export default function Home() {
+  const [isOrdered, setIsOrdered] = useState(false);
+  const titleProps = useSpring({
+    backgroundColor: isOrdered ? "white" : "black",
+    color: isOrdered ? "black" : "white",
+    config: config.slow,
+  });
+
+  const orderSummary1Props = useSpring({
+    opacity: isOrdered ? 1 : 0,
+    delay: 500,
+    config: config.slow,
+  });
+  const orderSummary2Props = useSpring({
+    opacity: isOrdered ? 1 : 0,
+    delay: 1000,
+    config: config.slow,
+  });
+
   return (
     <div className={styles.root}>
       <Head>
@@ -11,20 +31,36 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className={styles.heading}>Your Order</h1>
-      <h2 className={styles.mainContent}>Pre-Ordered!</h2>
-      <div className={styles.img}>
-        <Image src="" width={327} height={253} />
-      </div>
-      <div className={styles.orderSummary}>
-        <div className={styles.heading}>Order Summary</div>
-        <div className={styles.info}>
-          <div>Item #033322</div>
-          <div>$403.04</div>
+
+      <div>
+        <animated.div
+          style={titleProps}
+          className={styles.preOrder}
+          onClick={() => setIsOrdered(true)}
+        >
+          <h2 className={styles.mainContent}>
+            {isOrdered ? "Pre-Ordered!" : "Pre-Order →"}
+          </h2>
+        </animated.div>
+        <div className={styles.img}>
+          <Image src="/boot.png" width={351} height={253} />
         </div>
-      </div>
-      <div className={styles.delivery}>
-        <div className={styles.heading}>Expected Delivery</div>
-        <div className={styles.info}>October 30th, 2022</div>
+        <div>
+          <animated.div
+            style={orderSummary1Props}
+            className={styles.orderSummary}
+          >
+            <div className={styles.subHeading}>Order Summary</div>
+            <div className={styles.info}>
+              <div>Item #033322</div>
+              <div>$403.04</div>
+            </div>
+          </animated.div>
+          <animated.div style={orderSummary2Props} className={styles.delivery}>
+            <div className={styles.subHeading}>Expected Delivery</div>
+            <div className={styles.info}>October 30th, 2022</div>
+          </animated.div>
+        </div>
       </div>
     </div>
   );
